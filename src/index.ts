52714aa7d8c3a193e1537b91f27ef8fedcaea0c5
dipeshrai123 <raidipesh78@gmail.com>
2020-12-29 16:17:38 +0545

    Export index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
-export * from "./Animation";
-export * from "./Hooks";
-export * from "./Modules";
-export * from "./Interpolation";
-export * from "./Math";
+/**
+ * Modules
+ */
+export { AnimatedBlock, ScrollableBlock } from "./Modules";
+
+/**
+ * Animated Values
+ */
+export { useAnimatedValue, useMountedValue } from "./Animation";
+
+/**
+ * Hooks
+ */
+export {
+  useDrag,
+  useMeasure,
+  useOutsideClick,
+  useScroll,
+  useWindowDimension,
+} from "./Hooks";
+
+/**
+ * Interpolation
+ */
+export { interpolate, bInterpolate } from "./Interpolation";
+
+/**
+ * Clamp
+ */
+export { clamp, mix, rubberClamp } from "./Math";
