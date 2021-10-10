c13f15596456f2042bafaefb1c71630b215605e1
dipeshrai123 <raidipesh78@gmail.com>
2021-10-10 12:50:23 +0545

    useDrag() hook receives optional second paramenter as object with initial function

--- src/hooks/useDrag.ts
+++ src/hooks/useDrag.ts
@@ -1,10 +1,11 @@
 import * as React from "react";
-import { DragEventType, Vector2 } from "../Types";
+import { DragEventType, Vector2, UseDragConfig } from "../Types";
 import { clamp } from "../Math";
 import { attachEvents } from "../controllers";
 
 export function useDrag(
   callback: (event: DragEventType) => void,
+  config?: UseDragConfig,
   deps?: React.DependencyList
 ) {
   const _VELOCITY_LIMIT = 20;
@@ -23,6 +24,7 @@ export function useDrag(
   const movement = React.useRef<Vector2>({ x: 0, y: 0 });
   const movementStart = React.useRef<Vector2>({ x: 0, y: 0 });
   const previousMovement = React.useRef<Vector2>({ x: 0, y: 0 });
+  const initialMovement = React.useRef<Vector2>({ x: 0, y: 0 });
 
   // Holds offsets
   const translation = React.useRef<Vector2>({ x: 0, y: 0 });
@@ -106,6 +108,27 @@ export function useDrag(
           currentIndex.current = _refElementsMultiple.indexOf(currElem);
         }
 
+        // if initial function is defined then call it to get initial movementX and movementY
+        // if only select to bounded draggable element
+        const initial = config?.initial && config.initial();
+        const initialMovementX = initial?.movementX;
+        const initialMovementY = initial?.movementY;
+
+        initialMovement.current = {
+          x: initialMovementX ?? 0,
+          y: initialMovementY ?? 0,
+        };
+
+        movement.current = {
+          x: initialMovement.current.x,
+          y: initialMovement.current.y,
+        };
+
+        previousMovement.current = {
+          x: initialMovement.current.x,
+          y: initialMovement.current.y,
+        };
+
         handleCallback();
       }
     };
@@ -120,13 +143,19 @@ export function useDrag(
 
         if (e.type === "touchmove") {
           movement.current = {
-            x: e.touches[0].clientX - movementStart.current.x,
-            y: e.touches[0].clientY - movementStart.current.y,
+            x:
+              initialMovement.current.x +
+              (e.touches[0].clientX - movementStart.current.x),
+            y:
+              initialMovement.current.y +
+              (e.touches[0].clientY - movementStart.current.y),
           };
         } else {
           movement.current = {
-            x: e.clientX - movementStart.current.x,
-            y: e.clientY - movementStart.current.y,
+            x:
+              initialMovement.current.x + (e.clientX - movementStart.current.x),
+            y:
+              initialMovement.current.y + (e.clientY - movementStart.current.y),
           };
         }
 
@@ -147,6 +176,7 @@ export function useDrag(
             _VELOCITY_LIMIT
           ),
         };
+
         previousMovement.current = {
           x: movement.current.x,
           y: movement.current.y,
@@ -159,7 +189,6 @@ export function useDrag(
     const pointerUp = () => {
       if (isGestureActive.current) {
         isGestureActive.current = false;
-        velocity.current = { x: 0, y: 0 };
         handleCallback();
         _initEvents();
       }
