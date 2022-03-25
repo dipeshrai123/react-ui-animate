ea5571ff85d287496ab6b4bef2b758131257ab5f
dipeshrai123 <raidipesh78@gmail.com>
2022-03-25 19:28:08 +0545

    interpolate from re-motion

--- src/animation/interpolation.ts
+++ src/animation/interpolation.ts
@@ -1,42 +1,5 @@
-import {
-  interpolateNumbers,
-  interpolateTransitionValue,
-  ExtrapolateConfig,
-  isSubscriber,
-} from "@raidipesh78/re-motion";
-
-/**
- * interpolate function maps input range to output range
- * @param value - number | TransitionValue
- * @param inputRange - Array<number>
- * @param outputRange - Array<string | number>
- * @param extrapolateConfig - "clamp" | "identity" | "extend"
- * @returns - number | TransitionValue
- */
-export function interpolate(
-  value: any,
-  inputRange: Array<number>,
-  outputRange: Array<number | string>,
-  extrapolateConfig?: ExtrapolateConfig
-) {
-  if (typeof value === "object" && isSubscriber(value)) {
-    return interpolateTransitionValue(
-      value,
-      inputRange,
-      outputRange,
-      extrapolateConfig
-    );
-  } else if (typeof value === "number") {
-    return interpolateNumbers(
-      value,
-      inputRange,
-      outputRange,
-      extrapolateConfig
-    );
-  } else {
-    throw new Error(`Error! ${typeof value} cannot be interpolated`);
-  }
-}
+import { interpolate, ExtrapolateConfig } from '@raidipesh78/re-motion';
+export { interpolate } from '@raidipesh78/re-motion';
 
 /**
  * bInterpolate functions maps input range [0, 1] to given [minOutput, maxOutput]
