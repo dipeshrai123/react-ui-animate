6185ed68f8a9702ce2e8567e561e0c736c272834
Dipesh Rai <raidipesh78@gmail.com>
2022-05-29 12:54:44 +0545

    fix: typing fixes

--- src/animation/interpolation.ts
+++ src/animation/interpolation.ts
@@ -1,9 +1,28 @@
-import {
-  interpolate,
-  ExtrapolateConfig,
-  TransitionValue,
-} from '@raidipesh78/re-motion';
-export { interpolate } from '@raidipesh78/re-motion';
+import { ExtrapolateConfig, TransitionValue } from '@raidipesh78/re-motion';
+import { ValueType } from './useAnimatedValue';
+import { interpolate as internalInterpolate } from '@raidipesh78/re-motion';
+
+/**
+ * interpolate functions maps input range to given output range
+ * @param value - number | TransitionValue
+ * @param inputRange - Array<number>
+ * @param outputRange - Array<number | string>
+ * @param extrapolateConfig - "clamp" | "identity" | "extend"
+ * @returns - number | TransitionValue
+ */
+export function interpolate(
+  value: number | TransitionValue | ValueType,
+  inputRange: Array<number>,
+  outputRange: Array<number | string>,
+  extrapolateConfig?: ExtrapolateConfig
+) {
+  return internalInterpolate(
+    value as any,
+    inputRange,
+    outputRange,
+    extrapolateConfig
+  );
+}
 
 /**
  * bInterpolate functions maps input range [0, 1] to given [minOutput, maxOutput]
@@ -15,10 +34,15 @@ export { interpolate } from '@raidipesh78/re-motion';
  * @returns - number | TransitionValue
  */
 export function bInterpolate(
-  value: number | TransitionValue,
+  value: number | TransitionValue | ValueType,
   minOutput: number | string,
   maxOutput: number | string,
   extrapolateConfig?: ExtrapolateConfig
 ) {
-  return interpolate(value, [0, 1], [minOutput, maxOutput], extrapolateConfig);
+  return internalInterpolate(
+    value as any,
+    [0, 1],
+    [minOutput, maxOutput],
+    extrapolateConfig
+  );
 }
