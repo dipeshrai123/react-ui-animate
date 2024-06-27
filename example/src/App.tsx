15c93f0df55b19115f57e932303383d36f37e0dd
Dipesh Rai <raidipesh78@gmail.com>
2024-06-27 12:26:05 +0545

    fix: rollup typescript plugin disable

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import { AnimatedBlock, useAnimatedValue } from 'react-ui-animate';
 
 function App() {
-  const x = useAnimatedValue(0, { duration: 500, delay: 1000 });
+  const x = useAnimatedValue(0);
 
   return (
     <div>
@@ -14,10 +14,8 @@ function App() {
           left: x.value,
           top: 0,
         }}
-        onClick={() => (x.value = 100)}
-      >
-        HEY
-      </AnimatedBlock>
+        onClick={() => (x.value = 500)}
+      />
     </div>
   );
 }
