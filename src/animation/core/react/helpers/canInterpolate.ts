4f8d189cbd728c249b15970baa6c2807c4263112
Dipesh Rai <raidipesh78@gmail.com>
2024-06-27 16:17:14 +0545

    fix: refactor canInterpolate function

--- src/animation/core/react/helpers/canInterpolate.ts
+++ src/animation/core/react/helpers/canInterpolate.ts
@@ -3,12 +3,23 @@ import {
   getProcessedColor,
 } from '../../interpolation/Interpolation';
 
+/**
+ * Determines if two values can be interpolated.
+ * @param previousValue - The previous value to compare.
+ * @param newValue - The new value to compare.
+ * @returns True if interpolation is possible, false otherwise.
+ */
 export function canInterpolate(
   previousValue: number | string,
   newValue: number | string
-) {
-  if (typeof previousValue !== typeof newValue) return false;
-  if (typeof newValue === 'number') return true;
+): boolean {
+  if (typeof previousValue !== typeof newValue) {
+    return false;
+  }
+
+  if (typeof newValue === 'number') {
+    return true;
+  }
 
   if (typeof previousValue === 'string') {
     const processedPreviousValue = getProcessedColor(previousValue);
