1f513f4ce8ee64b92f9bd2c5284a83776796574c
Dipesh Rai <raidipesh78@gmail.com>
2024-07-01 20:47:52 +0545

    fix: update withFunction functions description

--- src/animation/lib/withFunctions.ts
+++ src/animation/lib/withFunctions.ts
@@ -5,6 +5,12 @@ import type { FluidValueConfig, Length } from '../core/types/animation';
 interface WithSpringConfig
   extends Pick<FluidValueConfig, 'mass' | 'friction' | 'tension'> {}
 
+/**
+ * Creates a spring animation configuration.
+ * @param {Length} toValue - The target value of the animation.
+ * @param {WithSpringConfig} [config=AnimationConfigUtils.ELASTIC] - Optional spring configuration.
+ * @returns {{ toValue: Length; config: WithSpringConfig }}
+ */
 export const withSpring = (
   toValue: Length,
   config: WithSpringConfig = AnimationConfigUtils.ELASTIC
@@ -18,6 +24,12 @@ export const withSpring = (
 interface WithTimingConfig
   extends Pick<FluidValueConfig, 'duration' | 'easing'> {}
 
+/**
+ * Creates a timing animation configuration.
+ * @param {Length} toValue - The target value of the animation.
+ * @param {WithTimingConfig} [config={ duration: 250 }] - Optional timing configuration.
+ * @returns {{ toValue: Length; config: WithTimingConfig }}
+ */
 export const withTiming = (
   toValue: Length,
   config: WithTimingConfig = { duration: 250 }
@@ -28,6 +40,11 @@ export const withTiming = (
   };
 };
 
+/**
+ * Creates a sequence of animations that run one after another.
+ * @param {Array<{ toValue: Length; config?: FluidValueConfig }>} configs - An array of animation configurations.
+ * @returns {Function} An async function that runs the animations in sequence.
+ */
 export const withSequence = ([...configs]: Array<{
   toValue: Length;
   config?: FluidValueConfig;
@@ -41,6 +58,12 @@ export const withSequence = ([...configs]: Array<{
   };
 };
 
+/**
+ * Adds a delay before the given animation.
+ * @param {number} delay - The delay in milliseconds.
+ * @param {{ toValue: Length; config?: FluidValueConfig }} animation - The animation configuration ( withTiming | withSpring )
+ * @returns {{ toValue: Length; config: FluidValueConfig }} The updated animation configuration with delay.
+ */
 export const withDelay = (
   delay: number,
   animation: { toValue: Length; config?: FluidValueConfig }
