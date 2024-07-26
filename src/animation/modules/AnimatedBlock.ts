69a3acd5870d7182fc44f18187619266ccdb089d
Dipesh Rai <raidipesh78@gmail.com>
2024-07-26 14:48:53 +0545

    fix: removed blocks

--- src/animation/modules/AnimatedBlock.ts
+++
@@ -1,8 +0,0 @@
-import { makeFluid } from '@raidipesh78/re-motion';
-
-/**
- * AnimatedBlock - A higher order component built upon `div` element
- * which can accept `AnimatedValue`. It also exposes some extra style properties like
- * translateX, translateY, rotateX, rotateY, scaleX, etc.
- */
-export const AnimatedBlock = makeFluid('div');
