536a611d85c221610da7bafe17a7dacabc68e24c
Dipesh Rai <raidipesh78@gmail.com>
2024-07-27 18:01:02 +0545

    fix: with functions config override issue fixed

--- src/animation/withFunctions.ts
+++ src/animation/withFunctions.ts
@@ -6,18 +6,6 @@ import type { UseFluidValueConfig } from './core/useFluidValue';
 interface WithOnCallbacks
   extends Pick<UseFluidValueConfig, 'onRest' | 'onStart' | 'onChange'> {}
 
-// Configuration interfaces
-interface WithEaseConfig extends WithOnCallbacks {}
-interface WithSpringConfig
-  extends Pick<UseFluidValueConfig, 'mass' | 'friction' | 'tension'>,
-    WithOnCallbacks {}
-interface WithTimingConfig
-  extends Pick<UseFluidValueConfig, 'duration' | 'easing'>,
-    WithOnCallbacks {}
-interface WithDecayConfig
-  extends Pick<UseFluidValueConfig, 'decay' | 'velocity' | 'deceleration'>,
-    WithOnCallbacks {}
-
 /**
  * Creates a default animation configuration.
  * @param {number} toValue - The target value of the animation.
@@ -29,16 +17,21 @@ export const withConfig = (toValue: number, config?: UseFluidValueConfig) => ({
   config,
 });
 
+// Configuration interfaces
+interface WithEaseConfig extends WithOnCallbacks {}
+
 /**
  * Creates an ease animation configuration.
  * @param {number} toValue - The target value of the animation.
  * @param {UseFluidValueConfig} [config=AnimationConfigUtils.EASE] - Optional ease configuration.
  * @returns {{ toValue: number; config: UseFluidValueConfig }}
  */
-export const withEase = (
-  toValue: number,
-  config: WithEaseConfig = AnimationConfigUtils.EASE
-) => withConfig(toValue, config);
+export const withEase = (toValue: number, config?: WithEaseConfig) =>
+  withConfig(toValue, { ...AnimationConfigUtils.EASE, ...config });
+
+interface WithSpringConfig
+  extends Pick<UseFluidValueConfig, 'mass' | 'friction' | 'tension'>,
+    WithOnCallbacks {}
 
 /**
  * Creates a spring animation configuration.
@@ -46,10 +39,12 @@ export const withEase = (
  * @param {WithSpringConfig} [config=AnimationConfigUtils.ELASTIC] - Optional spring configuration.
  * @returns {{ toValue: number; config: WithSpringConfig }}
  */
-export const withSpring = (
-  toValue: number,
-  config: WithSpringConfig = AnimationConfigUtils.ELASTIC
-) => withConfig(toValue, config);
+export const withSpring = (toValue: number, config?: WithSpringConfig) =>
+  withConfig(toValue, { ...AnimationConfigUtils.ELASTIC, ...config });
+
+interface WithTimingConfig
+  extends Pick<UseFluidValueConfig, 'duration' | 'easing'>,
+    WithOnCallbacks {}
 
 /**
  * Creates a timing animation configuration.
@@ -57,10 +52,12 @@ export const withSpring = (
  * @param {WithTimingConfig} [config={ duration: 250 }] - Optional timing configuration.
  * @returns {{ toValue: number; config: WithTimingConfig }}
  */
-export const withTiming = (
-  toValue: number,
-  config: WithTimingConfig = { duration: 250 }
-) => withConfig(toValue, config);
+export const withTiming = (toValue: number, config?: WithTimingConfig) =>
+  withConfig(toValue, { duration: 250, ...config });
+
+interface WithDecayConfig
+  extends Pick<UseFluidValueConfig, 'decay' | 'velocity' | 'deceleration'>,
+    WithOnCallbacks {}
 
 /**
  * Creates a decay animation configuration.
