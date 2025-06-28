7536ce4575cf5bd4c0498ce470d788e50d4775f7
Dipesh Rai <raidipesh78@gmail.com>
2025-06-28 23:31:34 +0545

    fix: examples updated

--- example/src/stories/gestures/hooks/useWheel/BasicSetup.tsx
+++ example/src/stories/gestures/hooks/useWheel/BasicSetup.tsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import { useWheel } from 'react-ui-animate';
 
 const Example = () => {
+  const [wheelPosition, setWheelPosition] = useState({ x: 0, y: 0 });
+
   useWheel(window, function (event) {
     console.log('Wheel event:', event);
+    setWheelPosition({ x: event.offset.x, y: event.offset.y });
   });
 
-  return <div style={{ height: 2000 }} />;
+  return (
+    <div style={{ height: 2000 }}>
+      <div style={{ position: 'fixed', left: 10, top: 10 }}>
+        WHEEL POSITION: {wheelPosition.x}, {wheelPosition.y}
+      </div>
+    </div>
+  );
 };
 
 export default Example;
