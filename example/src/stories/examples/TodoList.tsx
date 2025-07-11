5b4c0876526c63029c8c12aae2109391afffc87d
Dipesh Rai <raidipesh78@gmail.com>
2025-07-11 04:30:33 +0200

    style(todolist): updated the styling changes

--- example/src/stories/examples/TodoList.tsx
+++ example/src/stories/examples/TodoList.tsx
@@ -19,10 +19,45 @@ const TodoListItem = ({
       state={visible}
       exit={withSpring(0, { onComplete: () => onRemove() })}
     >
-      {(a) => (
-        <animate.div style={{ opacity: a }}>
-          <div onClick={() => setVisible(false)}>REMOVE</div>
-          {text}
+      {(opacity) => (
+        <animate.div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            opacity,
+            height: opacity.to([0, 1], [0, 40]),
+            scale: opacity.to([0, 1], [0.8, 1]),
+            width: 320,
+            overflow: 'hidden',
+          }}
+        >
+          <animate.div
+            style={{
+              border: '1px solid #3399ff',
+              borderRadius: 4,
+              borderTopRightRadius: 0,
+              borderBottomRightRadius: 0,
+              height: 40,
+              paddingLeft: 16,
+              display: 'flex',
+              alignItems: 'center',
+              flex: 1,
+            }}
+          >
+            {text}
+          </animate.div>
+          <button
+            style={{
+              border: '1px solid #3399ff',
+              borderLeftWidth: '0px',
+              borderTopLeftRadius: 0,
+              borderBottomLeftRadius: 0,
+              height: 40,
+            }}
+            onClick={() => setVisible(false)}
+          >
+            X
+          </button>
         </animate.div>
       )}
     </Mount>
@@ -42,16 +77,46 @@ const Example = () => {
 
   return (
     <div>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
-      <button onClick={saveTodo}>Save</button>
+      <input
+        style={{
+          padding: '10px 16px',
+          borderRadius: '4px',
+          border: '1px solid #ccc',
+          marginRight: 10,
+        }}
+        value={text}
+        placeholder="Enter todo..."
+        onChange={(e) => setText(e.target.value)}
+      />
 
-      {todos.map(({ id, text }) => (
-        <TodoListItem
-          key={id}
-          text={text}
-          onRemove={() => console.log('removed')}
-        />
-      ))}
+      <button
+        style={{ backgroundColor: '#3399ff', color: 'white', border: 'none' }}
+        onClick={saveTodo}
+      >
+        Add Todo
+      </button>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 10,
+          marginTop: 20,
+          marginBottom: 20,
+        }}
+      >
+        {todos
+          .map(({ id, text }) => (
+            <TodoListItem
+              key={id}
+              text={text}
+              onRemove={() =>
+                setTodos((prev) => prev.filter((p) => p.id !== id))
+              }
+            />
+          ))
+          .reverse()}
+      </div>
     </div>
   );
 };
