36379cd987c40a0cbbfaf473348dadc2a3b985f4
Dipesh Rai <raidipesh78@gmail.com>
2025-07-13 09:54:38 +0545

    docs(hooks): updated examples

--- example/src/stories/examples/InView.tsx
+++ example/src/stories/examples/InView.tsx
@@ -1,9 +1,32 @@
 import { useEffect, useRef } from 'react';
-import { useInView, useValue, animate, withSpring } from 'react-ui-animate';
+import {
+  useInView,
+  useValue,
+  animate,
+  withSpring,
+  useScroll,
+} from 'react-ui-animate';
 
-const Card = ({ i }: { i: number }) => {
+const COLORS = [
+  '#e4a3f1',
+  '#0b7c2d',
+  '#ff1a5b',
+  '#3f2d9e',
+  '#aacb47',
+  '#d01e8f',
+  '#5b9df4',
+  '#f4c610',
+  '#7e3a21',
+  '#12d8c3',
+];
+
+const Card = ({ i, color }: { i: number; color: string }) => {
   const ref = useRef<HTMLDivElement>(null);
   const isInView = useInView(ref);
+  const { scrollYProgress } = useScroll(window, {
+    target: ref,
+    offset: ['start center', 'start start'],
+  });
   const [progress, setProgress] = useValue(0);
 
   useEffect(() => {
@@ -11,17 +34,19 @@ const Card = ({ i }: { i: number }) => {
   }, [isInView, setProgress]);
 
   return (
-    <animate.div
-      ref={ref}
-      style={{
-        height: 300,
-        width: 300,
-        backgroundColor: '#3399ff',
-        opacity: progress,
-        scale: progress,
-        rotate: progress.to([0, 1], [-180, 0]),
-      }}
-    />
+    <animate.div style={{ scale: scrollYProgress.to([0, 1], [1, 2]) }}>
+      <animate.div
+        ref={ref}
+        style={{
+          height: 300,
+          width: 300,
+          backgroundColor: color,
+          opacity: progress,
+          scale: progress,
+          rotate: progress.to([0, 1], [-180, 0]),
+        }}
+      />
+    </animate.div>
   );
 };
 
@@ -30,7 +55,7 @@ const Example = () => {
     <div>
       <h1>In View Example</h1>
       <div style={{ height: '100vh' }} />
-      {Array.from({ length: 10 }).map((_, i) => (
+      {COLORS.map((color, i) => (
         <div
           key={i}
           style={{
@@ -38,9 +63,12 @@ const Example = () => {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
+            position: 'sticky',
+            top: 0,
+            zIndex: 1,
           }}
         >
-          <Card i={i} />
+          <Card i={i} color={color} />
         </div>
       ))}
     </div>
